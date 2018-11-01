3c58798201d83a15f28232dcd4c5a0c6025125be
samuel <samuelmanikiza@gmail.com>
2018-11-01 10:56:21 +0200

    Second Project Commit Setting up things only

---
+++ cons.php
@@ -0,0 +1,4 @@
+<?php 
+$global['root']=$_SERVER['DOCUMENT_ROOT'].'/pajo/main/';
+define("ROOT_URL", $global['root']);
+?>
--- test.php
+++ test.php
@@ -1,5 +1,4 @@
 <?php 
 require 'class_loader.php';
-//echo $util->field_query(Tables::users(),"user_id",2,"names");
-echo $util->field_sum(Tables::stock(),"quantity","product_id",3);
+
 ?>
